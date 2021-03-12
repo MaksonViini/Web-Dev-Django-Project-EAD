4b026096ba8a91fe82faba7aac33f13449d4bb34
MaksonViini <maksonvinicio7@gmail.com>
2021-03-11 22:00:04 -0300

    Adicionando o carregando da pagina especifica de cada curso

--- SimpleMooc/courses/urls.py
+++ SimpleMooc/courses/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from SimpleMooc.courses import views
 
 urlpatterns = [
-    path('cursos/', views.index, name='index')
+    path('cursos/', views.index, name='index'),
+    re_path(r'^(?P<slug>[\w-]+)/$', views.details, name='details')
 ]
--- SimpleMooc/courses/views.py
+++ SimpleMooc/courses/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Course
 
 # Create your views here.
@@ -9,5 +9,14 @@ def index(request):
     path = 'courses/index.html'
     context = {
         'courses': courses
+    }
+    return render(request, path, context)
+
+
+def details(request, slug):
+    course = get_object_or_404(Course, slug=slug)
+    path = 'courses/details.html'
+    context = {
+        'course': course
     }
     return render(request, path, context)
